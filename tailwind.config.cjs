b382818846a45353311d4e5c709942b3f3755d51
emrosas <emireles.rosas@gmail.com>
2022-10-01 12:14:18 -0500

    Added fonts and colors to tailwind.config file.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+      purple: { 100: "#5800FF", 200: "#3B00AA" },
+    },
+    fontFamily: {
+      title: ["teko", "sans-serif"],
+      sans: ["raleway", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
